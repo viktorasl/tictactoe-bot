2eebd40a92725116c5be836d8934f5b38b4ea1ae
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2015-11-03 17:15:45 +0200

    Cleared main module

--- src/Main.hs
+++ src/Main.hs
@@ -7,28 +7,23 @@ import Tictactoe.Decoder
 import Tictactoe.Att
 import Tictactoe.HTTPHelper
 
-import Network.HTTP
-
 gameURLStr :: String
 gameURLStr = "http://tictactoe.homedir.eu/game/randomgamenamex13/"
 
 main :: IO ()
 main = do
     --playAttacker gameURLStr
+    --playAttacker gameURLStr
     putStrLn "resp"
 
-mockResp :: IO String
-mockResp = do
-    return "ld1:v1:x1:xi1e1:yi1eee"
+playDefender :: String -> IO ()
+playDefender url = playDefender' url [ExpCenter, ExpAnyCorner]
 
-playDef :: IO ()
-playDef = playDef' [ExpCenter, ExpAnyCorner]
-
-playDef' :: [ExpectedMove Coords] -> IO ()
-playDef' scens = do
-    board <- getMove (gameURLStr ++ "player/2")
+playDefender' :: String -> [ExpectedMove Coords] -> IO ()
+playDefender' url scens = do
+    board <- getMove (url ++ "player/2")
     case def scens board of
         Just (field, scen) -> do
-            madeMove <- makeMove (gameURLStr ++ "player/2") (field : board)
-            playDef' [scen]
+            madeMove <- makeMove (url ++ "player/2") (field : board)
+            playDefender' url [scen]
         _ -> putStrLn "The game is finished"
