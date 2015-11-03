c8bf5eb7aa4ce62dbc1ee611a66532c719a0daa8
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2015-11-03 17:19:11 +0200

    Fixed url

--- src/Main.hs
+++ src/Main.hs
@@ -8,22 +8,22 @@ import Tictactoe.Att
 import Tictactoe.HTTPHelper
 
 gameURLStr :: String
-gameURLStr = "http://tictactoe.homedir.eu/game/randomgamenamex13/"
+gameURLStr = "http://tictactoe.homedir.eu/game/randomgamenamex18"
 
 main :: IO ()
 main = do
-    --playAttacker gameURLStr
-    --playAttacker gameURLStr
-    putStrLn "resp"
+    playDefender gameURLStr
+    playAttacker gameURLStr
+    --putStrLn "resp"
 
 playDefender :: String -> IO ()
 playDefender url = playDefender' url [ExpCenter, ExpAnyCorner]
 
 playDefender' :: String -> [ExpectedMove Coords] -> IO ()
 playDefender' url scens = do
-    board <- getMove (url ++ "player/2")
+    board <- getMove (url ++ "/player/2")
     case def scens board of
         Just (field, scen) -> do
-            madeMove <- makeMove (url ++ "player/2") (field : board)
+            madeMove <- makeMove (url ++ "/player/2") (field : board)
             playDefender' url [scen]
         _ -> putStrLn "The game is finished"
