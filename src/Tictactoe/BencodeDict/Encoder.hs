5200ad9aec41a5aec1d4270e5b8dbb6589051f13
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2015-11-09 00:20:29 +0200

    BencodeDict encoder

--- src/Tictactoe/BencodeDict/Encoder.hs
+++ src/Tictactoe/BencodeDict/Encoder.hs
@@ -3,6 +3,14 @@ module Tictactoe.BencodeDict.Encoder (
 ) where
 
 import Tictactoe.Base
+import Tictactoe.Bencode.Encoder (stringifyField)
 
 stringifyBoard :: Board -> String
-stringifyBoard board = ""
+stringifyBoard board = "d" ++ (stringifyBoardFields board 0 "") ++ "e"
+
+stringifyBoardFields :: Board -> Int -> String -> String
+stringifyBoardFields [] _ str = str
+stringifyBoardFields (field : left) num str = let
+    strField = stringifyField field
+    concatStr = (str ++ "1:" ++ (show num) ++ strField)
+    in stringifyBoardFields left (num + 1) concatStr
