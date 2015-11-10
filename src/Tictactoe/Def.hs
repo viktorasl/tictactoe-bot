d41130d54a1efb73b84e0e90400f1383aa16f919
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2015-11-10 11:32:25 +0200

    Defender content type in a method

--- src/Tictactoe/Def.hs
+++ src/Tictactoe/Def.hs
@@ -8,16 +8,19 @@ import Tictactoe.Bencode.Decoder
 import Tictactoe.HTTPHelper
 import Tictactoe.Move
 
+contentType :: String
+contentType = "application/bencode+list"
+
 playDefender :: String -> IO ()
 playDefender url = playDefender' url [ExpCenter, ExpAnyCorner]
 
 playDefender' :: String -> [ExpectedMove Coords] -> IO ()
 playDefender' url scens = do
-    board <- getMove (url ++ "/player/2") "application/bencode+list"
+    board <- getMove (url ++ "/player/2") contentType
     let
         parsedBoard = parseBoard board
         in case def scens parsedBoard of
             Just (field, scen) -> do
-                madeMove <- makeMove (url ++ "/player/2") "application/bencode+list" (stringifyBoard (field : parsedBoard))
+                madeMove <- makeMove (url ++ "/player/2") contentType (stringifyBoard (field : parsedBoard))
                 playDefender' url [scen]
             _ -> putStrLn "The game is finished"
